e4ccac71eb8527476a7480280fc312e78144cf44
Lionel <candieskhung@Lionels-Mac-Book.local>
2018-06-28 09:04:20 +0800

    Updated dice version with coin toss feature

--- code.py
+++ code.py
@@ -3,3 +3,12 @@ import random
 if __name__ == '__main__':
     print('The dice is {}'.format(random.randint(1,6)))
     print('It plays the dice!')
+
+    print("Let's play a coin toss!")
+
+    for i in range(1,4):
+        var = random.randint(0,1)
+        if var == 1:
+            print("It's head!")
+        if var == 0:
+            print("It's tail!")
