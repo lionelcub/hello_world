36f591607195f87b0f4db66bc2ae3e9191e3e253
lionelcub <36695608+lionelcub@users.noreply.github.com>
2018-06-28 09:28:43 +0800

    Added print statement for dice version

--- code.py
+++ code.py
@@ -3,3 +3,4 @@ import random
 if __name__ == '__main__':
     print('The dice is {}'.format(random.randint(1,6)))
     print('It plays the dice!')
+    print("Let's print again: {}".format(random.randint(1,6)))
