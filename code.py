c10ee885d887aee1af259066bb71b36e7598e8fe
Lionel <candieskhung@Lionels-Mac-Book.local>
2018-07-02 18:04:27 +0800

    Added 1 more dice

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 import random
 
 if __name__ == '__main__':
-    print('The dice is {}'.format(random.randint(1,6)))
-    print('It plays the dice!')
+    print('The dice is {}'.format(random.randint(1,12)))
+    print('It plays total number of 2 dices!')
